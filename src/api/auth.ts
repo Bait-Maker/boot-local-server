b356753459d074a85bd08238509cd181e31b9c6a
dejworj <dejworj@shure.com>
2026-03-03 15:58:02 -0600

    updated auth logic

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,12 +1,7 @@
 import { Request, Response } from "express";
 import { getUserByEmail } from "../db/queries/users.js";
 import { BadRequestError, UserUnauthorizedError } from "./customErrors.js";
-import {
-  checkPasswordHash,
-  getBearerToken,
-  makeJWT,
-  validateJWT,
-} from "../auth.js";
+import { checkPasswordHash, makeJWT, validateJWT } from "../auth.js";
 import { UserResponse } from "./users.js";
 import { respondWithJSON } from "./json.js";
 import { config } from "../config.js";
@@ -43,21 +38,21 @@ export async function handlerLogin(req: Request, res: Response) {
     throw new UserUnauthorizedError("incorrect email or password");
   }
 
-  let expiresIn = 3600; // 1hr
+  let duration = config.jwt.defaultDuration; // 1hr
 
-  if (params.expiresIn && params.expiresIn <= expiresIn) {
-    expiresIn = params.expiresIn;
+  if (params.expiresIn && !(params.expiresIn > config.jwt.defaultDuration)) {
+    duration = params.expiresIn;
   }
 
-  const token = makeJWT(user.id, expiresIn, config.jwt.secret);
+  const accessToken = makeJWT(user.id, duration, config.jwt.secret);
 
-  validateJWT(token, config.jwt.secret);
+  validateJWT(accessToken, config.jwt.secret);
 
   respondWithJSON(res, 200, {
     id: user.id,
     createdAt: user.createdAt,
     updatedAt: user.updatedAt,
     email: user.email,
-    token: token,
+    token: accessToken,
   } satisfies LoginResponse);
 }
